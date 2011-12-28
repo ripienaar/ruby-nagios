9b95d28ca6ef55a80f170439aa7fb4d0bef602fa
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2011-12-28 16:08:17 +0900

    Basic dynamic find_by_* method for searching resources (objects)

--- nagios/objects.rb
+++ nagios/objects.rb
@@ -18,8 +18,8 @@ Class implements 2 methods at the time of writing:
 
   require 'nagios/objects'
  
-   a = Nagios::Objects.new("test/objects.cache").new.parse
-   print a.objects[:contactgroup]
+   nagios = Nagios::Objects.new("test/objects.cache").new.parse
+   print nagios.objects[:contactgroup]
 
 == Files
 
@@ -32,6 +32,22 @@ On Debian system objects.cache it is in
 
   object_cache_file=/var/cache/nagios3/objects.cache
 
+== Parsed data hash
+
+    irb(main):010:0> pp nagios.objects
+     {:timeperiod=>
+      {"24x7"=>
+        {:timeperiod_name=>"24x7",
+         :alias=>"24 Hours A Day, 7 Days A Week",
+         :sunday=>"00:00-24:00",
+         :monday=>"00:00-24:00",
+         :tuesday=>"00:00-24:00",
+         :wednesday=>"00:00-24:00",
+         :thursday=>"00:00-24:00",
+         :friday=>"00:00-24:00",
+         :saturday=>"00:00-24:00"},
+       "never"=>{:timeperiod_name=>"never", :alias=>"Never"},
+
 = Author
 
 Dmytro Kovalov, dmytro.kovalov@gmail.com
@@ -42,6 +58,7 @@ Dmytro Kovalov, dmytro.kovalov@gmail.com
   class Objects
 
     # @param [String] path UNIX path to the objects.cache file
+    # @see Nagios::Objects.parse
     def initialize path
       raise "File does not exist" unless File.exist? path
       raise "File is not readable" unless File.readable? path
@@ -76,24 +93,21 @@ Example of a block:
           members root
           }
 
-Parsed data hash:
+Example of a parsed object:
 
-    irb(main):010:0> pp a.objects
-     {:timeperiod=>
-      {"24x7"=>
-        {:timeperiod_name=>"24x7",
-         :alias=>"24 Hours A Day, 7 Days A Week",
-         :sunday=>"00:00-24:00",
-         :monday=>"00:00-24:00",
-         :tuesday=>"00:00-24:00",
-         :wednesday=>"00:00-24:00",
-         :thursday=>"00:00-24:00",
-         :friday=>"00:00-24:00",
-         :saturday=>"00:00-24:00"},
-       "never"=>{:timeperiod_name=>"never", :alias=>"Never"},
-    
+    nagios.objects[:contactgroup]
+    => {"admins"=>{:contactgroup_name=>"admins", :alias=>"Nagios Administrators", :members=>"root"}}
+
+    nagios.contactgroup
+    => {"admins"=>{:contactgroup_name=>"admins", :alias=>"Nagios Administrators", :members=>"root"}}
+
+=== Convenience methods
+
+Method parse creates helper methods for every type of object after
+parsing. Same property can be accessed either using Hash @objects
+(i.e. nagios.objects[:host]) or convenience method: nagios.host.
+
 =end
-
     def parse
       block = {}
       content = File.readlines objects_file
@@ -116,8 +130,60 @@ Parsed data hash:
           block[$1.to_sym] = $2.strip
         end
       end
+
+      # Create instance methods for easy access to properties
+      @objects.each do |key,val|
+        instance_variable_set("@#{key}", val)
+        instance_eval "def #{key}; return #{val}; end"
+      end
+      self
     end
- 
+
+    # Basic find function for resources.
+    # @param [Symbol] resource Resource to search from: :host, :hostgroup, etc.
+    # @param [Symbol] attribute Attribute to use in search. For example, find host by hostname or address, etc. anything that's defined for this resource
+    # @param [Symbol] message  Is either 'find' or 'find_all' passed from caller. In case of 'find' returns 1 hash, 'find_all' - Array of Hash'es.
+    # @param [String] or [Regexp] pattern Search pattern
+    
+    def find resource, message, attribute, pattern
+      self.send(resource.to_sym).values.send(message) do |a| 
+        case pattern
+        when String
+          a[attribute.to_sym] == pattern
+        when Regexp
+          a[attribute.to_sym] =~ pattern
+        else
+          raise 'Unknown pattern for search'
+        end
+        
+      end
+    end
+
+    # Replace standard +method_missing+ with dynamic search methods. Calls internally self.find.
+    #
+    # @see find
+    #
+    # @param [Symbol] sym Should be in the form
+    #     find(_all)?_<resource>_by_<attribute>. Similar to
+    #     ActiveResource find_* dynamic methods. Depending on the name
+    #     of the mthod called (find or find_all) will pass message to
+    #     self.find method, that will call {Array.find} or
+    #     {Array.find_all} accordingly.
+    #
+    # find_*_by and find_all_*. find_all returns Array of
+    # hashes. 
+
+    def method_missing sym, *args, &block
+      raise(NoMethodError, "No such method #{sym.to_s} for #{self.class}") unless sym.to_s =~ /^(find(_all)?)_(.*)_by_(.*)$/
+      # message - either 'find' of 'find_all'
+      # resource - type of objects to search: host, hostgroup etc.
+      # attribute - name of the attribute to do search by: :host_name, :check_command
+      # @param *args String or Regexp to search objects
+      message,resource,attribute  = $1, $3, $4
+
+      self.find resource,message,attribute,args[0]
+    end
+
   end
 end
 
