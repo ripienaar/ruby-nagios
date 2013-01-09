1de89c73b91bfc82cc7a73d36ddf07384d77d7b0
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-01-09 11:27:54 +0900

    add ENV varaible and /usr/local to default configuration

--- config/default.rb
+++ config/default.rb
@@ -1,7 +1,11 @@
 module Nagios
 
   DEFAULT = { 
-    :nagios_cfg_glob => "/etc/nagios*/nagios.cfg"
+    :nagios_cfg_glob => ENV['NAGIOS_CFG_FILE'] || 
+    [
+     "/etc/nagios*/nagios.cfg", 
+     "/usr/local/nagios/etc/nagios.cfg"
+    ]
   }
 
 end
