8b0ed48c9b755d4b8fced860008706ed20c14205
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2013-01-29 18:17:36 +0900

    Retire configuration file config/default.rb, move code to lib/nagios.rb

--- lib/nagios.rb
+++ lib/nagios.rb
@@ -1,4 +1,15 @@
-require File.dirname(__FILE__) + '/../config/default.rb'
+module Nagios
+
+  DEFAULT = { 
+    :nagios_cfg_glob => ENV['NAGIOS_CFG_FILE'] || 
+    [
+     "/etc/nagios*/nagios.cfg", 
+     "/usr/local/nagios/etc/nagios.cfg"
+    ]
+  }
+
+end
+
 require 'nagios/config'
 require 'nagios/external_commands'
 require 'nagios/objects'
